33b0e2da283b0c81518ba849a9cbf48e4640b83e
tanaka.satoshi <tanaka.satsohi@a-tm.co.jp>
2020-07-24 22:21:16 +0900

    fix db

--- main.go
+++ main.go
@@ -22,7 +22,8 @@ func main() {
 	r.GET("/test", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"test":  "item",
-			"array": []int{1, 2, 3},
+			"array": []int{1, 2, 3, 4, 5},
+			"map":   map[string]int{"apple": 150, "banana": 300, "lemon": 300},
 		})
 	})
 	r.Run(":8080")
